1ef53c7cbf38d9b36181f6e94710200f2824914c
Alex Bergdahl <bergdahlalex@protonmail.com>
2026-05-20 10:01:24 +0200

    Update roslyn_ls

--- lua/plugins/lsp/servers/roslyn.lua
+++ lua/plugins/lsp/servers/roslyn.lua
@@ -1,11 +1,11 @@
 return {
-  "roslyn",
+  "roslyn_ls",
   category = "core",
   ft = "cs",
   after = function()
     if vim.fn.executable('Microsoft.CodeAnalysis.LanguageServer') == 1 then
-      vim.lsp.config('roslyn', {})
-      vim.lsp.enable('roslyn')
+      vim.lsp.config('roslyn_ls', {})
+      vim.lsp.enable('roslyn_ls')
     end
   end,
 }
