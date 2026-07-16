5586ac662f95b482b102596d053883f4a99a3dfb
Alex Bergdahl <bergdahlalex@protonmail.com>
2026-07-16 08:29:31 +0200

    Make tiny cmd line sit higher

--- lua/plugins/ui/tiny-cmdline.lua
+++ lua/plugins/ui/tiny-cmdline.lua
@@ -1,15 +1,19 @@
 return {
-  "tiny-cmdline-nvim",
-  category = "core",
-  event = "DeferredUIEnter",
-  before = function()
-    vim.o.cmdheight = 0
-    require('vim._core.ui2').enable({})
-  end,
-  after = function()
-    require("tiny-cmdline").setup({
-      on_reposition = require("tiny-cmdline").adapters.blink,
-      native_types = {},
-    })
-  end,
+   'tiny-cmdline-nvim',
+   category = 'core',
+   event = 'DeferredUIEnter',
+   before = function()
+      vim.o.cmdheight = 0
+      require('vim._core.ui2').enable({})
+   end,
+   after = function()
+      require('tiny-cmdline').setup({
+         position = {
+            x = '50%', -- horizontal: "0%" = left, "50%" = center, "100%" = right
+            y = '40%', -- vertical:   "0%" = top,  "50%" = center, "100%" = bottom
+         },
+         on_reposition = require('tiny-cmdline').adapters.blink,
+         native_types = {},
+      })
+   end,
 }
